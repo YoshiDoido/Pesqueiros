ad26ac450598b2159d5574d1c6bb5d8daf1fb303
Gabriel Yoshino Florencio <gabrielyoshinof@gmail.com>
2024-04-14 20:50:15 -0300

    Classe ClienteDTO criada

---
+++ src/main/java/com/fatecdiadema/pesqueiros/pesqueiros/model/ClienteDTO.java
@@ -0,0 +1,9 @@
+package com.fatecdiadema.pesqueiros.pesqueiros.model;
+
+public class ClienteDTO {
+    String nome;
+    String cpf;
+    String email;
+    String senha;
+    String nrTelefone;
+}
