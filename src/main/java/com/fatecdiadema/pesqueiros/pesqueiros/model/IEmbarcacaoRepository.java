8a70a39dbe636804098be0520f0072f5a2b5ac9d
Gabriel Yoshino Florencio <gabrielyoshinof@gmail.com>
2024-04-14 21:14:52 -0300

    Classe IEmbarcacaoRepository criada

---
+++ src/main/java/com/fatecdiadema/pesqueiros/pesqueiros/model/IEmbarcacaoRepository.java
@@ -0,0 +1,9 @@
+package com.fatecdiadema.pesqueiros.pesqueiros.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IEmbarcacaoRepository extends JpaRepository<Embarcacao, Long> {
+
+}
