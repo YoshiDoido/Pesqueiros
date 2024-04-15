840822f72bda9274387c4fa114cb6b2ef0a5bd46
Gabriel Yoshino Florencio <gabrielyoshinof@gmail.com>
2024-04-14 21:08:07 -0300

    Classe EmbarcacaoDTO criada

---
+++ src/main/java/com/fatecdiadema/pesqueiros/pesqueiros/model/EmbarcacaoDTO.java
@@ -0,0 +1,9 @@
+package com.fatecdiadema.pesqueiros.pesqueiros.model;
+
+public class EmbarcacaoDTO {
+    String proprietario;
+    String nomeEmbarcacao;
+    String arrais;
+    String imagem;
+    Integer capacidade;
+}
