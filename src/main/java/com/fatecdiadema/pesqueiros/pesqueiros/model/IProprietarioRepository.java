476448523c97325e1a601c9f4b1f7d9c485f0c70
Gabriel Yoshino Florencio <gabrielyoshinof@gmail.com>
2024-04-20 17:15:16 -0300

    IProprietarioRepository criado

---
+++ src/main/java/com/fatecdiadema/pesqueiros/pesqueiros/model/IProprietarioRepository.java
@@ -0,0 +1,6 @@
+package com.fatecdiadema.pesqueiros.pesqueiros.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IProprietarioRepository extends JpaRepository<Proprietario, Long> {
+}
